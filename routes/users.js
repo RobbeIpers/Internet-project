59db0cf7501405ed9e9bca4b6cbe47d5318b9a03
fransvetspek <38458351+fransvetspek@users.noreply.github.com>
2018-12-01 14:05:32 +0100

    account
    account pug en navbar enal

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,32 @@ var router = express.Router();
 var user_controller = require('../controllers/userController');
 var passport= require('passport');
 var bcrypt= require('bcryptjs');
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/account',function(req,res,next){
+    res.render('account',{title: req.user.name});
+});
+
+
 //login form
 router.get('/login', function(req, res, next) {
     var flashMessages = res.locals.getMessages();
     console.log('flash', flashMessages);
-    
-    if(flashMessages.error){
-        res.render('login', {
-            showErrors: true,
-            errors: flashMessages.error
-        });
-    }
-    else{
-        res.render('login', {title: 'Log In'});
-    }
+        if(flashMessages.error){
+            res.render('login', {
+                showErrors: true,
+                errors: flashMessages.error
+            });
+        }
+        else{
+            res.render('login', {title: 'Log In'});
+        }
+
 });
+
 //login process
 router.post('/login', function(req, res, next){
     passport.authenticate('local',{
@@ -48,10 +53,11 @@ router.get('/register', function(req, res, next) {
         res.render('register', {title: 'Register'});
     }
 });
+
 router.post('/register', user_controller.user_create_post);
 
 router.get('/logout', function(req, res, next) {
-  if (req.session) {
+  if (req.session) { 
     // delete session object
     req.session.destroy(function(err) {
       if(err) {
@@ -62,5 +68,6 @@ router.get('/logout', function(req, res, next) {
     });
   }
 });
+
 
 module.exports = router;
