33654d5fbc466fe4fa4c78d45f47755e9e81e86a
fransvetspek <38458351+fransvetspek@users.noreply.github.com>
2018-12-04 20:51:38 +0100

    ejjj

---
+++ models/stem.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+//Define a schema
+var Schema = mongoose.Schema;
+
+var Stem = new Schema({
+    titel          : {
+        type: String,
+        required: true
+    },
+    artiestNaam    : {
+        type: String, required: true
+    },
+    email    : {
+        type: Number,
+        default: 1
+    }
+});
+
+Stem
+.virtual('url')
+.get(function () {
+  return '/homepage/stem/' + this._id;
+});
+
+module.exports = mongoose.model('Stem', Stem );
