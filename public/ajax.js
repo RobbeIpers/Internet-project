97910cbad04492843b6beff75042cffbf8800ff7
elienvanassche <44496117+elienvanassche@users.noreply.github.com>
2018-12-08 16:43:30 +0100

    homepage done

--- public/ajax.js
+++ public/ajax.js
@@ -1,6 +1,7 @@
 function showHint(str){
     if(str.length==0){
         document.getElementById("idveld").innerHTML = "";
+        document.getElementById("idveld2").innerHTML = "";
         return;
     }        
     else{
@@ -9,13 +10,16 @@ function showHint(str){
             if (this.readyState == 4 && this.status == 200){ 
                 var table="<tr class='w3-red'><th>Titel</th><th>Artiest</th><th>Stemmen</th></tr>";
                 if(JSON.parse(this.responseText).length == 0){
-                    document.getElementById("idveld").innerHTML= "Je liedje heeft nog geen stemmen. Misschien een typefout? Of gewoon slechte smaak...";    
+                    label="Je liedje heeft nog geen stemmen. Misschien een typefout? Of gewoon slechte smaak...";
+                    document.getElementById("idveld2").innerHTML= label; 
+                    document.getElementById("idveld").innerHTML = "";
                 }
                 else{
                     for(i=0;i<JSON.parse(this.responseText).length;i++){
                         table += "<tr><td>" +JSON.parse(this.responseText)[i].titel + "</td><td>" + JSON.parse(this.responseText)[i].artiestNaam +"</td><td>"+JSON.parse(this.responseText)[i].aantStemmen +"</td></tr>";
                     }
                     document.getElementById("idveld").innerHTML = table;
+                    document.getElementById("idveld2").innerHTML = "";
                 }
             }
         };
