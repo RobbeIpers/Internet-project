d0c4bea5ea093b4a541e11cb71dd3262dc6bf8c5
elienvanassche <44496117+elienvanassche@users.noreply.github.com>
2018-12-08 11:15:55 +0100

    layout top10 in orde

--- public/ajax.js
+++ public/ajax.js
@@ -7,7 +7,7 @@ function showHint(str){
         xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200){ 
-                var table="<tr><th>Titel</th><th>Artiest</th><th>AantalStemmen</th></tr>";
+                var table="<tr class='w3-red'><th>Titel</th><th>Artiest</th><th>Stemmen</th></tr>";
                 for(i=0;i<JSON.parse(this.responseText).length;i++){
                     table += "<tr><td>" +JSON.parse(this.responseText)[i].titel + "</td><td>" + JSON.parse(this.responseText)[i].artiestNaam +"</td><td>"+JSON.parse(this.responseText)[i].aantStemmen +"</td></tr>";                
                     
