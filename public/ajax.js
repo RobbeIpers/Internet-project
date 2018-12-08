b21c08a12aa4fda601f5ef26aa56c056a96ca4ad
fransvetspek <38458351+fransvetspek@users.noreply.github.com>
2018-12-08 13:03:22 +0100

    Update ajax.js

--- public/ajax.js
+++ public/ajax.js
@@ -9,14 +9,13 @@ function showHint(str){
             if (this.readyState == 4 && this.status == 200){ 
                 var table="<tr class='w3-red'><th>Titel</th><th>Artiest</th><th>Stemmen</th></tr>";
                 if(JSON.parse(this.responseText).length == 0){
-                document.getElementById("idveld").innerHTML= "";    
+                    document.getElementById("idveld").innerHTML= "";    
                 }
                 else{
-                for(i=0;i<JSON.parse(this.responseText).length;i++){
-                    table += "<tr><td>" +JSON.parse(this.responseText)[i].titel + "</td><td>" + JSON.parse(this.responseText)[i].artiestNaam +"</td><td>"+JSON.parse(this.responseText)[i].aantStemmen +"</td></tr>";                
-                    
-                }
-                document.getElementById("idveld").innerHTML = table;
+                    for(i=0;i<JSON.parse(this.responseText).length;i++){
+                        table += "<tr><td>" +JSON.parse(this.responseText)[i].titel + "</td><td>" + JSON.parse(this.responseText)[i].artiestNaam +"</td><td>"+JSON.parse(this.responseText)[i].aantStemmen +"</td></tr>";
+                    }
+                    document.getElementById("idveld").innerHTML = table;
                 }
             }
         };
